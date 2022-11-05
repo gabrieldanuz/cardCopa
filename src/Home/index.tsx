d1d324c79978702b0ce1f7145bb2b3b355350837
Gabriel Oliveira <gabrieldanuzoliveira358@gmail.com>
2022-11-05 10:13:00 -0300

    Finalizado

---
+++ src/Home/index.tsx
@@ -0,0 +1,83 @@
+import { useState, useEffect, useRef } from 'react';
+import { Camera, CameraType } from 'expo-camera';
+import { Image, SafeAreaView, ScrollView, TextInput, View, TouchableOpacity, Text } from 'react-native';
+import * as Sharing from 'expo-sharing';
+import {captureRef} from 'react-native-view-shot';
+
+import { Header } from '../components/Header';
+import { Button } from '../components/Button';
+import { PositionChoice } from '../components/PositionChoice';
+
+import { styles } from './styles';
+import { POSITIONS, PositionProps } from '../utils/positions';
+
+export function Home() {
+  const [photo, setPhotoURI] = useState<null | string>(null);
+  const [hasCameraPermission, setHasCameraPermission] = useState(false);
+  const [positionSelected, setPositionSelected] = useState<PositionProps>(POSITIONS[0]);
+
+  const cameraRef = useRef<Camera>(null);
+  const screenShotRef = useRef(null);
+
+
+  async function handleTakePicture(){
+    const photo = await cameraRef.current.takePictureAsync();
+    setPhotoURI(photo.uri);
+  }
+
+  async function shareScreenShot(){
+    const screenShot = await captureRef(screenShotRef);
+    await Sharing.shareAsync("file://" + screenShot);
+  }
+
+  useEffect(() => {
+    Camera.requestCameraPermissionsAsync()
+    .then(response => setHasCameraPermission(response.granted));
+  },[]);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scroll} showsVerticalScrollIndicator={false}>
+        <View style={styles.sticker} 
+              ref={screenShotRef}>
+          <Header position={positionSelected} />
+
+          <View style={styles.picture}>
+
+            {
+              hasCameraPermission && !photo ? 
+              <Camera 
+              ref={cameraRef}
+                style={styles.camera}
+                type={CameraType.front}
+              /> :
+              <Image 
+              source={{ uri: photo ? photo : 'https://filestore.community.support.microsoft.com/api/images/490b996b-e45f-4985-b2af-cf36da33849a?upload=true'}} 
+              style={styles.camera} 
+              onLoad={shareScreenShot}
+              />
+            }
+
+            <View style={styles.player}>
+              <TextInput
+                placeholder="Digite seu nome aqui"
+                style={styles.name}
+              />
+            </View>
+          </View>
+        </View>
+
+        <PositionChoice
+          onChangePosition={setPositionSelected}
+          positionSelected={positionSelected}
+        />
+
+        <TouchableOpacity onPress={() => setPhotoURI(null)}>
+          <Text style={styles.retry}>Nova Foto</Text>
+        </TouchableOpacity>
+
+        <Button title="Compartilhar" onPress={handleTakePicture}/>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
